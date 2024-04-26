0e2e429efd85a9cb9d328dac780166bdcf7bd43c
mithun30052001 <lmithun01@gmail.com>
2024-04-26 17:28:28 +0530

    unwanted components removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,14 @@ import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
-import {ConfirmationDialogComponent} from './confirmation-dialog/confirmation-dialog.component';
-import { AlertDialogComponent } from './alert-dialog/alert-dialog.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { DummyComponent } from './dummy/dummy.component';
-import { NavigationDialogComponent } from './navigation-dialog/navigation-dialog.component';
 import { MatDialogComponent } from './mat-dialog/mat-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConfirmationDialogComponent,
-    AlertDialogComponent,
     DummyComponent,
-    NavigationDialogComponent,
     MatDialogComponent
   ],
   imports: [
